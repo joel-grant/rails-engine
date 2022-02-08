ea8edc55a043028db64380acf2951f054d5b3712
Joel Grant <joelmgrant@me.com>
2022-02-07 20:53:25 -0700

    Test: Create basic test for locating one merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,4 +17,14 @@ describe "Merchants API" do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+  end
 end
