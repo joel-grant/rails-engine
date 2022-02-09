e697b361fb4d8aaa78ef686a0cb305493ac12062
Joel Grant <joelmgrant@me.com>
2022-02-09 12:20:15 -0700

    Add functionality to alphabetize merchant names

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,6 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def self.search_merchant(keyword)
-    where("Name  ILIKE ?", "%#{keyword}%").first
+    where("Name  ILIKE ?", "%#{keyword}%").order(:name).first
   end
 end
